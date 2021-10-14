54a0d69fafc120cd3e25e6794ed2de4c5620da3c
Yash Panchal <61722945+YashPanchal07@users.noreply.github.com>
2021-10-14 16:31:06 +0530

    Webengage script change

--- js/resp.js
+++ js/resp.js
@@ -9,3 +9,22 @@ burger.addEventListener('click',()=>{
     navList.classList.toggle('v-class-resp')
 
 })
+
+var btn = document.getElementById('btn');
+            btn.addEventListener('click',submitFunc = ()=> {
+         
+                var firstname = document.getElementById("firstname").value || "";                      
+                var lastname = document.getElementById("lastName").value || "";
+                var phone = document.getElementById("phone").value || "";
+                var email = document.getElementById("email").value || "";
+              
+
+                console.log({firstname, lastname, phone, email});
+
+                webengage.user.login(email);
+                webengage.user.setAttribute('we_first_name', firstname);
+                webengage.user.setAttribute('we_last_name', lastname );
+                webengage.user.setAttribute('we_phone', phone);
+                webengage.user.setAttribute('we_email', email);
+                
+            })
